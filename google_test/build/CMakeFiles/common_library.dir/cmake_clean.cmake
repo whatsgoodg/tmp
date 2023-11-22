28599b7fe93bbc540b6192175eacfcbd8c898d87
whatsgoodg <rlwjddl1596@naver.com>
2023-11-22 16:09:26 +0000

    google tested double_left_rotation() and tested well.

---
+++ google_test/build/CMakeFiles/common_library.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/common_library.dir/library_code.cc.o"
+  "CMakeFiles/common_library.dir/library_code.cc.o.d"
+  "libcommon_library.a"
+  "libcommon_library.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/common_library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
